4b024fa031251fbeacb6ecf2821d8d027d59de0d
Matt Coleman <matt@datto.com>
2016-12-03 15:25:43 -0500

    Increase HIVEX_MAX_SUBKEYS and HIVEX_MAX_VALUES
    This increases the defined limits based on counts observed in the
    Microsoft\Windows NT\CurrentVersion subkey of the software hive.

--- lib/hivex-internal.h
+++ lib/hivex-internal.h
@@ -323,8 +323,8 @@ extern int _hivex_get_values (hive_h *h, hive_node_h node, hive_value_h **values
   } while (0)
 
 /* These limits are in place to stop really stupid stuff and/or exploits. */
-#define HIVEX_MAX_SUBKEYS       25000
-#define HIVEX_MAX_VALUES        10000
+#define HIVEX_MAX_SUBKEYS       70000
+#define HIVEX_MAX_VALUES        55000
 #define HIVEX_MAX_VALUE_LEN   8000000
 #define HIVEX_MAX_ALLOCATION  1000000
 
