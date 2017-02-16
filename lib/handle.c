8e187357f466c31a9e75ac4924b32bbf4823e73f
Dawid Zamirski <dzamirski@datto.com>
2017-02-16 18:17:24 -0500

    lib: allow to walk registry with corrupted blocks
    Only when HIVEX_OPEN_UNSAFE flag is set.
    
    There are some corrupted registry files that have invalid hbin cells
    but are still readable. This patch makes the following changes:
    
    * hivex_open - do not abort with complete failure if we run across a
      block with invalid size (unless it's the root block). Instead just
      log the event, and move on. This will allow open hives that have
      apparent invalid blocks but the ones of potential interest might be
      perfectly accessible.
    * _hivex_get_children - similiarly, if the's invalid subkey, just skip
      it instead of failing so one can continue to browse other valid
      subkeys.
    
    The above is similar to the behavior to Windows regedit where one can
    load such corrupted hives with e.g. "reg load HKU\Corrupted" and
    browse/change it despite some keys might be missing.
    
    (cherry picked from commit 5345ec8f1df304dc93a6b1e4cbb40e7c1cf6f88d)

--- lib/handle.c
+++ lib/handle.c
@@ -313,10 +313,18 @@ hivex_open (const char *filename, int flags)
       int used;
       seg_len = block_len (h, blkoff, &used);
       if (seg_len <= 4 || (seg_len & 3) != 0) {
-        SET_ERRNO (ENOTSUP,
-                   "%s: block size %" PRIu32 " at 0x%zx, bad registry",
-                   filename, le32toh (block->seg_len), blkoff);
-        goto error;
+        if (is_root || !h->unsafe) {
+          SET_ERRNO (ENOTSUP,
+                     "%s, the block at 0x%zx has invalid size %" PRIi32
+                     ", bad registry",
+                     filename, blkoff, le32toh (block->seg_len));
+          goto error;
+        } else {
+          DEBUG (2,
+                 "%s: block at 0x%zx has invalid size %" PRIi32 ", skipping",
+                 filename, blkoff, le32toh (block->seg_len));
+          break;
+        }
       }
 
       if (h->msglvl >= 2) {
